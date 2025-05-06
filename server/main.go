48fc4174d082829666c4a145558c320f44e54cb1
Mwai Banda <bandamwai@gmail.com>
2025-05-05 23:53:45 -0500

    first commit

---
+++ server/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+	
+	"github.com/labstack/echo/v4"
+)
+
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, []string{
+			"https://m8-final-bucket.s3.us-east-1.amazonaws.com/13439846_1920_1080_25fps.mp4",
+			"https://m8-final-bucket.s3.us-east-1.amazonaws.com/13432587_3840_2160_30fps.mp4",
+			"https://m8-final-bucket.s3.us-east-1.amazonaws.com/12981408_3840_2160_25fps.mp4",
+			"https://m8-final-bucket.s3.us-east-1.amazonaws.com/13489700_3840_2160_30fps.mp4",
+		})
+	})
+	e.Logger.Fatal(e.Start(":80"))
+}
